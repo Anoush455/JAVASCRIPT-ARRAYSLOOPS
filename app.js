5cbd32360b91f21611711c5e128841bf94b97cda
Anoush khan <143010760+Anoush455@users.noreply.github.com>
2023-09-18 15:47:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,222 @@
+// chp # 17-20(ARRAYS AND LOOP);
+
+// Q # 3;
+// for (var input = 1; input <= 10; input++) {
+//     console.log(input);
+// }
+
+
+// // Q # 4;
+// var userinput = +prompt("Enter a number to show its multiplication table");
+
+// var tablength = +prompt("Enter a table length");
+
+// for (var i = 1; i <= tablength; i++) {
+//     document.write(userinput + "x" + i + "=" + userinput * i + "<br>");
+// }
+
+
+// //  Q # 5;
+// var fruits = ["apple" , "banana" , "mango" , "orange" , "strawberry"];
+// console.log(fruits);
+
+// for(var i = 0; i < fruits.length; i++){
+//     console.log("element of " + fruits[i] + "at" + "" + i);
+// }
+
+
+// // Q # 6;
+// console.log("Counting:");
+// for (var i = 1; i <= 15; i++){
+//     console.log(i);
+// }
+
+// console.log("Reverse Counting:");
+// for (var i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+// console.log("Even");
+// for (var i = 0; i <=20; i += 2 ){
+//     console.log(i);
+// }
+
+// console.log("Odd");
+// for (var i = 1; i <= 19; i += 2){
+//     console.log(i)
+// }
+
+// console.log("Series");
+// for(var k = 1; k <= 10; k++) {
+//     console.log(2 * k + "k");
+// }
+
+
+// // Q # 7;
+// var arr  = ["cake" , "apple pie", "cookie", "chips", "patties"];
+// console.log(arr)
+
+// var userinput = prompt("Welcome to ABC Bakery.What do you want to order sirma'am?")
+// alert("cookie is available at index 2 in our bakery");
+
+// var userinput = prompt("Welcome to ABC Bakery.What do you want to order sirma'am?")
+// alert("We are sorry. pastry is not available in our bakery");
+
+
+// // Q # 8;
+// var arritems = [24, 53, 78, 91, 12];
+// console.log(arritems);
+
+// var arr = [24, 53, 78, 91, 12];
+// var largest = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest ) {
+//     largest = arr[i];
+//   }
+// }
+// console.log(largest);
+
+// // Q # 9;
+// var arritems = [24, 53, 78, 91, 12];
+// console.log(arritems);
+
+// const numbers = [24, 53, 78, 91, 12];
+
+// const smallest_number = Math.min(...numbers);
+
+// console.log('Smallest Value:', smallest_number);
+
+
+// // Q # 10;
+// for ( var i = 1; i <= 100; i++ )
+// {
+//   if ( i%3 === 0 && i%5 === 0 )
+//   {
+//     console.log( i);
+//   }
+//   else if ( i%3 === 0 ) 
+//   {
+//     console.log(i);
+//   }
+//   else if ( i%5 === 0 ) 
+//   {
+//     console.log(i);
+//   }
+//   else
+//   {
+//     console.log(i);
+//   }
+// }
+
+// ****************** CLASS ASSIGMENT COMPLETED ****************
+
+// Q # 1;
+// for (var i = 1; i <= 10; i++) {
+//     console.log("Welcome user", i);
+// }
+
+
+// Q # 2;
+// for (var i = 0; i < 12; i++) {
+//   let name = "John Doe";
+//   console.log("Hi, my name is " + name);
+// }
+
+
+// Q # 3;
+// for (var i = 0; i <= 4; i++){
+//   console.log(i);
+// }
+
+
+// Q # 4;
+
+// for (var count = 0; count < 100; count++) {
+//     // Your code here
+//     // This loop will run 100 times
+//     console.log(count)
+// }
+
+
+// Q # 5
+// for (var i = 3; i > 0; i--) {
+//     console.log(i)
+//     // Your code here
+//     // This loop will run 3 times, decrementing i with each iteration
+// }
+
+
+// Q # 6;
+// var array = [1, 2, 3, 4, 5];
+// var length = array.length;{
+//     console.log(length);
+// }
+
+
+// Q # 7;
+// var flag = true;{
+//     console.log(flag);
+// }
+
+
+// Q # 8;
+// for (var i = 0; i < pets.length; i++) {
+//     // Your code here
+//     // This loop will run as many times as there are elements in the "pets" array
+// }
+
+
+// Q # 9;
+// for (var i = 0; i < 10; i++) {
+//     if (i === 1) {
+//         alert("Counter is: " + i);
+//         break;
+//     }
+// }
+
+
+// Q # 10;
+// var username = ["Ali" , "Aliyan" , "Abdullah" ,"Hamza"];
+
+// var firstname = prompt("Enter first name");
+
+// for (var i = 0; i < username.length; i++) {
+//     if (firstname === username[i]) {
+//         alert("Welcome, " + firstname + "");
+//         break;
+//     }
+//     if (i === username.length - 1) {
+//         alert("please write the correct user name.");
+//     }
+// }
+
+
+// Q # 11;
+// var matchFound = false;
+
+// for (var i = 0; i < matchFound; i++) {
+//     if (userInput === list[i]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+// }
+
+// if (!matchFound) {
+//     alert("Match not found");
+// }
+
+
+// Q # 12;
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+//     for (var j = 0; j < secondArr.length; j++) {
+//         console.log(firstArr[i] + secondArr[j]);
+//     }
+// }
+
+
+///    ============ THE END ==================  ///
